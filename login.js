fd1b1ef04a3c1409b87df0ab5ca389dac600a346
Syl <laracheramy@gmail.com>
2025-02-20 11:05:29 +0100

    comment on password method

--- login.js
+++ login.js
@@ -18,16 +18,16 @@ $form.addEventListener("submit", (e) => {
     $notif.classList.remove("hidden");
     setTimeout(() => {
       $notif.classList.add("hidden");
-    }, 2000);
+    }, 2000); //au bout d'un délais de 2000ms, la classe hidden est de nouveau ajoutée
   }
 });
 
 // je dois créer des constantes pour mes valeurs personnelles de login et de password, que j'appelle mylogin et mypassword
 // je dois créer des constantes en cherchant les input $password et $login en les appelant par leur classe pour pouyvoir les comparer plus tard
-//je dois créer une constante bouton pour pouvoir écouter le click dessus plus tard
+//je dois créer une constante form pour pouvoir écouter le submit dessus plus tard
 //je dois créer des constantes pour chaque éléments ou je vais retirer ou ajouter la classe hidden
 
-//je mets un event listener sur mon click de bouton
-// ensuite je fais un if et else
+//je mets un event listener sur mon submit de form
+// ensuite je fais un if et else qui compare mes avleurs de mypassword et de mylogin aux valeurs des champs des inputs $password et $login (du coup je dois écrire $password.value)
 
 //    $notif.classList.add("hidden").delay(3000);
